13b7342d8872da1dbe268f51b8f06f0a74eae919
mohamad shabane arani <51704066+shabane@users.noreply.github.com>
2020-11-13 19:28:38 +0330

    Add files via upload

--- bingwall/bingwall.py
+++ bingwall/bingwall.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-# BingWall Is A Simple Script To Download Daily WallPaper Of www.bing.com (Base On Date Of The Day)
+# BingWall Is A Simple Script To Download Daily WallPaper Of www.bing.com
 # I Hope Have Fun And Beauty Backgroud :)
 # Please Visit My GitHub At https://github.com/shabane
 
 import requests
 import datetime
-import time
 from pathlib import Path
 import pathlib
 import os
 
-YMD_Now = "!"
-global YMD_LastDay 
-YMD_LastDay = ""
-
-
 def Idown():
     
-    usrhome =str(Path.home())
-
     x = datetime.datetime.now()
     
+    login = os.path.expanduser('~')
+
     Date = str(x.year)+"/"+str(x.month)+"/"+str(x.day)+" "+str(x.hour)+":"+str(x.minute)+":"+str(x.second)
 
     JsonUrl = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
@@ -38,36 +32,31 @@ def Idown():
     url = "https://www.bing.com/"+tmp3['url']
 
     r = requests.get(url)
-    print("file download")
-    os.mkdir(usrhome+"/Downloads/bingwall/")
-    print("dir maked")
-    with open(usrhome+"/Downloads/bingwall/"+name+".jpg", 'wb') as f:
+    
+    with open(login+"/Pictures/"+name+".jpg", 'wb') as f:
         f.write(r.content)
-        print("file saved")
-    # Save Download Log To Main Directory  
-    file = open("/var/log/bingwall/"+"bingwall.log","a")
-    file.write("---------------------------------------- New WallPaper Downloaded ----------------------------------------"+"\n")
+
+    # Save Download Log 
+    file = open(login+"/Pictures/"+"bingwall.log","a")
+    file.write("_________New WallPaper Downloaded"+"\n")
     file.write("Date        > "+Date+"\n")
     file.write("Url         > https://www.bing.com/"+tmp3['url']+"\n")
     file.write("Copyright   > "+tmp3['copyright']+"\n")
     file.write("Hash/Name   > "+name+"\n")
     file.close()
-    
-    global YMD_LastDay
-    YMD_LastDay = str(x.year)+"/"+str(x.month)+"/"+str(x.day)
 
-while(True):
-    NDate = datetime.datetime.now()
-    YMD_Now = str(NDate.year)+"/"+str(NDate.month)+"/"+str(NDate.day)
-    if(YMD_Now != YMD_LastDay):
-        time.sleep(60)
-        try:
-            Idown()
-        except:
-            # Save Faild Log
-            print("Some Fucking Thing Wrong ! Shit")
-            file = open("/var/log/bingwall/"+"bingwall.log","a")
-            file.write("------------------- Fail To Download WallPaper _ Auto ReTry At 11 Minute -------------------"+"\n")
-            file.write("Date        > "+YMD_Now+" "+str(NDate.hour)+":"+str(NDate.minute)+":"+str(NDate.second)+"\n")
-            file.close()
-    time.sleep(600)
+
+
+try: 
+    Idown()
+except Exception as e: # Save Faild Log
+    login = os.path.expanduser('~')
+    x = datetime.datetime.now()
+
+    file = open(login+"/Pictures/"+"bingwall.log","a")
+    file.write("Err______Faild To Download"+"\n")
+    file.write("Date        > "+str(x.year)+"/"+str(x.month)+"/"+str(x.day)+" "+str(x.hour)+":"+str(x.minute)+":"+str(x.second)+"\n")
+    file.write("Message     > "+e)
+
+
+    
--- bingwall/install.sh
+++ bingwall/install.sh
@@ -1,11 +1,5 @@
-#!/usr/bin/bash
-sudo mkdir /var/log/bingwall
-sudo chmod o+rwx /var/log/bingwall
-sudo mkdir /opt/bingwall
-sudo cp bingwall.py /opt/bingwall
 sudo apt-get install python3-pip
 pip3 install -r lib.txt
-sudo cp bingwall.service /lib/systemd/system
-sudo systemctl daemon-reload
-systemctl enable bingwall.service
-sudo systemctl start bingwall.service
+sudo cp bingwall.py /etc/cron.daily
+sudo chmod +x /etc/cron.daily/bingwall.py
+echo "Have Fun"
