c3e5692d47fe2d79e646bdd56df87512f1698d92
mohamad shabane arani <51704066+shabane@users.noreply.github.com>
2021-02-22 15:28:41 +0330

    Add files via upload

--- bingwall.py
+++ bingwall.py
@@ -4,16 +4,20 @@ import json
 import requests
 
 url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+print("Sending request")
 req = requests.get(url)
 if(req.status_code == 200):
+    print("Request sent succes")
     j = json.loads(req.content)
     imageUrl = "https://bing.com" + j["images"][0]["url"]
     imageName =  j["images"][0]["hsh"] + ".jpg"
     req = requests.get(imageUrl)
+    print("Downloading . . .")
     if(req.status_code == 200):
-        fli = open(imageName, "wb")
+        fli = open("picture/"+imageName, "wb")
         fli.write(req.content)
+        print("image saved at %s" %(imageName))
     else:
-        print("err in download the image")
+        print("err in download image or saveing")
 else:
     print("err in request the url:", req.status_code)
