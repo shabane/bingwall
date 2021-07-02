cbe2d9d87b1ca59a6cf1421ad88a7ad73d6c062f
shabane <m.mohamadshabane@gmail.com>
2021-07-02 16:56:41 +0430

    the bingwall hase a lot of change suddenly:
    now we log what bingwall do. at first we log the date and time then we
    log the request to the server.
    
    and if bingwall could not connect to the server we try angain and again
    and again. ....

--- bingwall.py
+++ bingwall.py
@@ -1,23 +1,38 @@
 #!/bin/python3
 
+import logging
 import json
 import requests
+import datetime
 
-url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
-print("Sending request")
-req = requests.get(url)
-if(req.status_code == 200):
-    print("Request sent succes")
-    j = json.loads(req.content)
-    imageUrl = "https://bing.com" + j["images"][0]["url"]
-    imageName =  j["images"][0]["hsh"] + ".jpg"
-    req = requests.get(imageUrl)
-    print("Downloading . . .")
+date = datetime.datetime.now()
+logging.basicConfig(filename='/var/log/bingwall.log', encoding='utf-8', level=logging.DEBUG)
+
+def main():
+    url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+    print("Sending request")
+    logging.debug('----------------' + str(date) + '----------------')
+    req = requests.get(url)
     if(req.status_code == 200):
-        fli = open("picture/"+imageName, "wb")
-        fli.write(req.content)
-        print("image saved at %s" %(imageName))
+        print("Request sent succes")
+        j = json.loads(req.content)
+        imageUrl = "https://bing.com" + j["images"][0]["url"]
+        imageName =  j["images"][0]["hsh"] + ".jpg"
+        req = requests.get(imageUrl)
+        print("Downloading . . .")
+        logging.debug('starting to download')
+        if(req.status_code == 200):
+            fli = open("/home/loading/Pictures/"+imageName, "wb")
+            fli.write(req.content)
+            print("image saved at %s" %(imageName))
+            logging.debug(f'image saved at {imageName}')
+        else:
+            print("err in download image or saveing")
+            logging.error('fail to download')
+            main()
     else:
-        print("err in download image or saveing")
-else:
-    print("err in request the url:", req.status_code)
+        print("err in request the url:", req.status_code)
+        logging.error('fail to connect to bing.com')
+        main()
+
+main()
